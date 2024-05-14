832a77022e72232a81062c97a3d0c50a353464cd
Eraldo <eraldoitalogones@gmail.com>
2024-05-14 16:36:54 -0300

    Reaprendendo JavaScript

---
+++ desafio01.js
@@ -0,0 +1,13 @@
+// supondo que: 
+// valor do litro de conbustivel: 5.22/l
+// gasto medio do carro: 12/l
+// distancia da percorrida: 250km
+
+const preco = 5.22;
+let media = 12;
+const distancia = 250; 
+
+let val1 = preco * distancia;
+let result = val1 / media; 
+
+console.log(result)
