7d59deb16aca7bf18a07e778b023518a0c36a057
eraldoitalo <eraldo.div@gmail.com>
2024-05-16 16:05:30 -0300

    Add files via upload

---
+++ 01 - variaveis e operadores/aula01.js
@@ -0,0 +1,26 @@
+let variavel = 10;
+    // a variável de let/var podem ter seus valores alterados
+console.log(variavel)
+    // caso eu queira alerar, basta:
+variavel = 20;
+console.log(variavel)
+
+// -----------------------------------------------------
+
+const valor = 5;
+console.log(valor)
+    // o valor de uma const nao pode ser alterado
+valor = 10;
+    // caso seja alterado o terminal me mostrará um erro
+
+// -------------------------OPERADORES---------------------
+//   + soma 
+//   - subtracao 
+//   * multiplicacao 
+//   / divisao
+
+let val1 = 27;
+let val2 = 35;
+let result = val1 + val2;
+console.log (result) 
+
